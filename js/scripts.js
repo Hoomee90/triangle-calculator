95957efa1285801bff032875f4a0611900069ecf
Samantha Callie <Hoomee90@gmail.com>
2023-08-24 14:29:54 -0700

    add triangle calculator and form output text while totally pretending I didn't just finish the project having forgotten to make basically any commits

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
-function calculateTriangle(sideA, sideB, sideC) {
-
+//Business Logic
+function calculateTriangle(a, b, c) {
+  return a && b && c ?
+  a + b <= c || a + c <= b || c + b <= a ? "not a triangle"
+  : a === b && b === c ? "equilateral"
+  : a === b || a === c || b === c ? "isosceles"
+  : "scalene" : "nonexistent";
 }
 
+//UI logic
 window.addEventListener("load", function() {
   const form = document.querySelector("form");
 
   form.addEventListener("submit", function(event) {
-    event.preventDefault;
-  });
+    event.preventDefault();
+    const sideAInput = parseInt(document.querySelector("#sideA").value);
+    const sideBInput = parseInt(document.querySelector("#sideB").value);
+    const sideCInput = parseInt(document.querySelector("#sideC").value);
+    const outputNode = document.querySelector("h3#output");
+
+    const result = calculateTriangle(sideAInput, sideBInput, sideCInput);
+    outputNode.innerHTML = "That triangle is " + result;
+  })
 });
