d4cedb16c4b9f985cfa5fdcc22786dd386a474fc
Samantha Callie <Hoomee90@gmail.com>
2023-08-24 13:21:39 -0700

    create js with basic event listeners

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function calculateTriangle(sideA, sideB, sideC) {
+
+}
+
+window.addEventListener("load", function() {
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault;
+  });
+});
